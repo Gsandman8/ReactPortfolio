583d9e4b4a3f7ff6e2bc2211ac5f207f12f3f7b3
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-19 11:02:14 -0800

    Added router setup to main.jsx and basic component order to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/NavTabs';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <div className="App">
-    </div>
-  )
+    <>
+      <Header />
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
