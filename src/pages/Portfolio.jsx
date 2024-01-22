4895418f65607b188dbbe0d5a46980f5b2240873
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-22 13:18:16 -0800

    Fixed header for portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,20 +5,22 @@ import MR from "../assets/MRSS.png";
 function Portfolio() {
     return (
         <div className="container-fluid">
-            <h1>My Portfolio</h1>
+            <div className="container">
+                <h1 className="text-center">My Portfolio</h1>
+            </div>
             <div className="row">
-            <Project
-                title="Meal and Reel"
-                description="A website that utilizies the Yelp and OMDB APIs to search for restaurants and movie suggestions."
-                image={MR}
-                link="https://gsandman8.github.io/ReelAndMeal/"
-            />
-            <Project
-                title="Social Goals Tracker"
-                description="A full-stack aplicaiton that allows users to create and track goals."
-                image={SGT}
-                link="https://sleepy-headland-38825-e68e0897078f.herokuapp.com/"
-            />
+                <Project
+                    title="Meal and Reel"
+                    description="A website that utilizies the Yelp and OMDB APIs to search for restaurants and movie suggestions."
+                    image={MR}
+                    link="https://gsandman8.github.io/ReelAndMeal/"
+                />
+                <Project
+                    title="Social Goals Tracker"
+                    description="A full-stack aplicaiton that allows users to create and track goals."
+                    image={SGT}
+                    link="https://sleepy-headland-38825-e68e0897078f.herokuapp.com/"
+                />
             </div>
         </div>
     );
