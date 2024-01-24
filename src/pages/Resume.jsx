07752d934e40e931ad1271478776661267f00eb6
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-24 12:19:32 -0800

    Added basic resume page layout

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,29 @@
 function Resume(){
     return(
-        <div>
+        <div className="justify-content-center text-center row">
             <h1 className="text-center" style={{fontFamily:"Helvetica", padding:"20px" }}>Resume</h1>
             <hr />
+            <div className="container col-12 row justify-content-center">
+                <div className="col-4">
+                    <h3 className="text-center">Front-End Proficiencies</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="col-4">
+                    <h3 className="text-center">Back-End Proficiencies</h3>
+                    <ul>
+
+                    </ul>
+                </div>
+                <div className="col-4">
+                    <h3 className="text-center">Other Skills</h3>
+                    <ul>
+
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
